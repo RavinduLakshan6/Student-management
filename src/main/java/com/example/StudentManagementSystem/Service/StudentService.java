7a7c358abc95a1da3f7a77339c9de6c6f8185abe
Ravindu Lakshan <lakshan-ps20159@stu.kln.ac.lk>
2024-11-07 23:28:36 +0530

    first commit

---
+++ src/main/java/com/example/StudentManagementSystem/Service/StudentService.java
@@ -0,0 +1,12 @@
+package com.example.StudentManagementSystem.Service;
+import com.example.StudentManagementSystem.model.Student;
+import java.util.List;
+
+public interface StudentService {
+    Student saveStudent(Student student);
+    List<Student>getAllStudent();
+    Student getStudentById(long id);
+    Student updateStudent(Student student,long id);
+    void deleteStudent(long id);
+
+}
